1f21bf3644495e0470e64bac451b84e913a3d319
GameCrucible <cpdepalm@gmail.com>
2023-03-18 02:11:25 -0500

    CannonFix
    Added: Cannon now only fires when there's mouse movement.
    Attempted to instantiate cannonballs in front of cannon
    
    Need to; Add the menus
    Make levels

--- Protect Your King/Assets/Scripts/CannonFire.cs
+++ Protect Your King/Assets/Scripts/CannonFire.cs
@@ -9,30 +9,38 @@ public class CannonFire : MonoBehaviour
     public Vector3 raycastCollision = Vector3.zero;
     public bool isProjectile;
     private Vector3 animateScale = new Vector3(0f, 0f, 0.5f);
-
+    private Vector3 spawnSpot = new Vector3(0, 0, 50);
+    public Vector3 lastMousePosition;
     void Start()
     {
         isProjectile = false;
+        lastMousePosition = Input.mousePosition;
     }
     void FixedUpdate()
     {
         Vector3 detect = transform.TransformDirection(Vector3.forward);
-        if (Physics.Raycast(transform.position, detect, 100))
+        if (Input.mousePosition != lastMousePosition)
         {
-            if(isProjectile == false)
+            lastMousePosition = Input.mousePosition;
+            if (Physics.Raycast(transform.position, detect, 100))
             {
-                print("fire");
-                isProjectile = true;
-                this.transform.localScale -= animateScale;
-                Invoke("fireCannon", 2);
+                if (isProjectile == false)
+                {
+                    print("fire");
+                    isProjectile = true;
+                    this.transform.localScale -= animateScale;
+                    Invoke("fireCannon", 2);
+                }
             }
         }
+        
+        
 
     }
 
     void fireCannon()
     {
-        GameObject projectile = Instantiate<GameObject>(Projectile);
+        GameObject projectile = Instantiate<GameObject>(Projectile,spawnSpot, Quaternion.identity);
         projectile.transform.position = transform.position;
         this.transform.localScale += animateScale;
         Invoke("delayFire", 3);
